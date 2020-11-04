55fb837751b3263310203fa4dafd155dcfff8d75
Alina <alinamiha293@gmail.com>
2020-11-04 22:30:00 +0200

    site is done

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,8 @@
-$('.carousel').carousel({
-    interval: 1500
+// $('.carousel').carousel({
+//     interval: 1500
+// })
+$(".burger-menu").on('click', function () {
+    $(this).toggleClass('active-burger')
+    $('.header_menu').toggleClass('mobile-header-visible')
+    $('.header-menu-list').toggleClass('open')
 })
