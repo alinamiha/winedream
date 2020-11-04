5954a5b9f4f4a74348fe7fa2dedba6c405e7aa91
Alina <alinamiha293@gmail.com>
2020-11-04 22:30:44 +0200

    uncommenting some code

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,6 @@
-// $('.carousel').carousel({
-//     interval: 1500
-// })
+$('.carousel').carousel({
+    interval: 1500
+})
 $(".burger-menu").on('click', function () {
     $(this).toggleClass('active-burger')
     $('.header_menu').toggleClass('mobile-header-visible')
